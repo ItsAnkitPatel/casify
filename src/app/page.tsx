42695b34c8ff979cff99827173a4dbd0dab4a2a6
ItsAnkitPatel <itsakppatel@gmail.com>
2024-05-29 22:05:16 +0530

    add customer review subsection in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
 import { Check, Star } from "lucide-react";
@@ -117,6 +118,115 @@ export default function Home() {
             </div>
           </div>
         </MaxWidthWrapper>
+      </section>
+
+      {/* Value proposition section: Basically what our customer say about the product*/}
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our{" "}
+              <span className="relative px-2">
+                customers
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+              </span>
+              say
+            </h2>
+
+            <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            {/* User review 1 */}
+            <div className="flex flex-auto gap-4 flex-col lg:pr-8 xl:pr-20">
+              {/* stars */}
+              <div className="flex gap-0.5 mb-2">
+                <Star className="size-5 text-green-600 fill-green-600" />
+                <Star className="size-5 text-green-600 fill-green-600" />
+                <Star className="size-5 text-green-600 fill-green-600" />
+                <Star className="size-5 text-green-600 fill-green-600" />
+                <Star className="size-5 text-green-600 fill-green-600" />
+              </div>
+
+              {/* Reviewer's Text */}
+              <div className="text-lg leading-8">
+                <p>
+                  "The case feels durable and I even got a compliment on the
+                  design. Had the case for two and a half month now and{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    the image is super clear
+                  </span>
+                  , on the case I had before, the image started fading into
+                  yellow-ish color after a couple weeks. Love it"
+                </p>
+              </div>
+
+              {/* Reviewer's Info subsection */}
+              <div className="flex gap-4 mt-2">
+                {/* Reviewer Image */}
+                <img
+                  className="rounded-full size-12 object-cover"
+                  src="/users/user-1.png"
+                  alt="user"
+                />
+                {/* Reviewer name */}
+                <div className="flex flex-col">
+                  <p className="font-semibold">Johnathan</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="size-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* User review 2*/}
+            <div className="flex flex-auto gap-4 flex-col lg:pr-8 xl:pr-20">
+              {/* stars */}
+              <div className="flex gap-0.5 mb-2">
+                <Star className="size-5 text-green-600 fill-green-600" />
+                <Star className="size-5 text-green-600 fill-green-600" />
+                <Star className="size-5 text-green-600 fill-green-600" />
+                <Star className="size-5 text-green-600 fill-green-600" />
+                <Star className="size-5 text-green-600 fill-green-600" />
+              </div>
+
+              {/* Reviewer's Text */}
+              <div className="text-lg leading-8">
+                <p>
+                  "I usually keep my phone together with my keys in my pocket
+                  and that led to some pretty heavy scratchmarks on all of my
+                  last phone cases. This one, beside a barely noticeable scratch
+                  on the corner,{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    looks brand new after about half a year
+                  </span>
+                  . I dig it."
+                </p>
+              </div>
+              {/* Reviewer's Info subsection */}
+              <div className="flex gap-4 mt-2">
+                {/* Reviewer Image */}
+                <img
+                  className="rounded-full size-12 object-cover"
+                  src="/users/user-4.jpg"
+                  alt="user"
+                />
+
+                {/* Reviewer name */}
+                <div className="flex flex-col">
+                  <p className="font-semibold">Josh</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="size-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+
+
       </section>
     </div>
   );
