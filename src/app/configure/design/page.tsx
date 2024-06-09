d754cc05acc310ea84a6fcf13544817713cc60a9
ItsAnkitPatel <itsakppatel@gmail.com>
2024-06-09 12:08:58 +0530

    build: Step-2 image dragging/cropping feature done

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,5 +1,35 @@
-const page = async () => {
-    
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+const page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+  // make db call
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, croppedImageUrl, height, width } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imageDimensions={{ width, height }}
+      imageUrl={imageUrl}
+    />
+  );
 };
 
 export default page;
---
+++ src/components/HandleComponent.tsx
@@ -0,0 +1,7 @@
+const HandleComponent = () => {
+  return (
+    <div className="size-5 rounded-full border border-zinc-500 bg-white shadow transition hover:bg-primary" />
+  );
+};
+
+export default HandleComponent;
